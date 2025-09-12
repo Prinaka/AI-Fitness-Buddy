e895dcc7799c87b36c90c1c30ba529d2b92a7c37
Prinaka Chakraborty <prinaka.4002@gmail.com>
2025-09-12 14:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
 import streamlit as st
 from fitness_chatbot import build_or_load_vectorstore, generate_answer
 
-st.set_page_config(page_title="Health & Fitness Assistant", layout="wide")
+st.set_page_config(page_title="Health & Fitness Assistant")
 
-st.markdown("""
-    <style>
-        /* Center all headings and text */
-        .center-text {
-            text-align: center;
-        }
-
-        /* Chat-like answer box */
-        .answer-box {
-            border-radius: 5px;
-            font-size: 1.1em;
-        }
-
-        /* Make textarea wider and centered */
-        .stTextArea textarea {
-            text-align: left;
-            font-size: 1.1em;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-st.markdown("<h1 class='center-text'>AI Fitness Buddy</h1>", unsafe_allow_html=True)
-st.markdown("<p class='center-text'>Ask about nutrition, workouts, or guidelines!</p>", unsafe_allow_html=True)
+st.title("AI Fitness Buddy")
+st.write("Ask about nutrition, workouts, or guidelines!")
 st.divider()
 
 @st.cache_resource
@@ -41,8 +20,3 @@ if query:
         response = generate_answer(vs, query)
     chatbot = st.chat_message("assistant")
     chatbot.write(response)
-
-
-
-
-
